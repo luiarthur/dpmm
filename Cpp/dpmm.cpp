a276673fa0a3b3441616a28d09068f658304747b
Arthur Lui <luiarthur@gmail.com>
2016-12-02 08:29:12 -0800

    yes

--- Cpp/dpmm.cpp
+++ Cpp/dpmm.cpp
@@ -26,3 +26,11 @@ double metropolis(double curr, double(*ll)(double), double(*lp)(double), double 
 
   return out;
 }
+
+// works with -std=c+=11
+//#include<functional>
+//double manip2(double x, std::function<double(double)> fn) { return fn(x); }
+//manip2(2, [](double x){return x+1;})
+//// Enable C++11 via this plugin (Rcpp 0.10.3 or later)
+//// // [[Rcpp::plugins("cpp11")]]
+////

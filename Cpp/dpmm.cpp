a6a3c7e87613b8513b1f486a98ec8eec380102cf
Arthur Lui <luiarthur@gmail.com>
2016-12-01 23:06:31 -0800

    yes

--- Cpp/dpmm.cpp
+++ Cpp/dpmm.cpp
@@ -1,1 +1,28 @@
+#include <Rcpp.h>
 
+using namespace Rcpp;
+
+//[[Rcpp::export]]
+double logit(double p) {
+  return log(p / (1-p));
+}
+
+//[[Rcpp::export]]
+double invLogit(double x) {
+  return 1 / (1 + exp(-x));
+}
+
+double metropolis(double curr, double(*ll)(double), double(*lp)(double), double stepSig)
+{
+  double cand = R::rnorm(curr,stepSig);
+  double u = R::runif(0,1);
+  double out;
+
+  if (ll(cand) + lp(cand) - ll(curr) - lp(curr) > log(u)) {
+    out = cand;
+  } else {
+    out = curr;
+  }
+
+  return out;
+}
---
+++ Cpp/main.R
@@ -0,0 +1,3 @@
+library(Rcpp)
+
+sourceCpp("dpmm.cpp")

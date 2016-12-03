5233a605f2094ca3e1b6e40cd63980c74daa7fb6
Arthur Lui <luiarthur@gmail.com>
2016-12-03 14:06:03 -0800

    Binomial DP: B+burn = 2000+10000 = 12000
    Rcpp:   0.768s
    scala:  2.836s
    julia: 23.262s

--- Cpp/main.R
+++ Cpp/main.R
@@ -13,3 +13,5 @@ system.time(out <- fit(y,m,alpha=1,cs=1,B=2000,burn=10000,printEvery=0))
 plot(p,col='grey',pch=20,ylim=c(0,1))
 points(y/m, col='red',pch=20)
 points(apply(out,1,mean),col='blue',lwd=2)
+
+

a3691ff98aeab660786b28924b695599de78d13a
Arthur Lui <luiarthur@gmail.com>
2016-12-03 12:27:29 -0800

    yes

--- Cpp/dpmm.cpp
+++ Cpp/dpmm.cpp
@@ -1,5 +1,6 @@
 #include<Rcpp.h>
 #include<functional> // std::function
+#include<map>
 
 using namespace Rcpp;
 
@@ -7,12 +8,10 @@ using namespace Rcpp;
 // [[Rcpp::plugins("cpp11")]]
 
 
-//[[Rcpp::export]]
 double logit(double p) {
   return log(p / (1-p));
 }
 
-//[[Rcpp::export]]
 double invLogit(double x) {
   return 1 / (1 + exp(-x));
 }
@@ -35,8 +34,8 @@ double metropolis(double curr, std::function<double(double)> ll, std::function<d
 double metLogit(double curr, std::function<double(double)> ll, std::function<double(double)> lp, double stepSig)
 {
   auto lp_logit = [lp](double logit_p) { // capture lp in []
-    double p = invLogit(logit_p);
-    double log_J = -logit_p + 2 * log(p);
+    const double p = invLogit(logit_p);
+    const double log_J = -logit_p + 2 * log(p);
     return lp(p) + log_J;
   };
   auto ll_logit = [ll](double logit_p) { return ll(invLogit(logit_p)); };
@@ -44,9 +43,10 @@ double metLogit(double curr, std::function<double(double)> ll, std::function<dou
 }
 
 
-int wsample_index(NumericVector p) { // GOOD
-  double p_sum = std::accumulate(p.begin(), p.end(), 0.0);
-  double u = R::runif(0,p_sum);
+int wsample_index(double p[], int n) { // GOOD
+  const double p_sum = std::accumulate(p, p+n, 0.0);
+  const double u = R::runif(0,p_sum);
+
   int i = 0;
   double cumsum = 0;
 
@@ -58,9 +58,10 @@ int wsample_index(NumericVector p) { // GOOD
   return i-1;
 }
 
-double wsample(NumericVector x, NumericVector p) { // GOOD
-  return x[wsample_index(p)];
+double wsample(double x[], double p[], int n) { // GOOD
+  return x[wsample_index(p,n)];
 }
+
 
 NumericVector algo8(double alpha, NumericVector t, 
                     std::function<double(double,int)> lf,
@@ -72,7 +73,76 @@ NumericVector algo8(double alpha, NumericVector t,
                                          double)> mh,
                     double cs, double clusterUpdates) {
   auto f = [lf](double x, int i){ return exp(lf(x,i)); };
-  int n = t.size();
+  const int n = t.size();
+  std::vector<double> newT(t.begin(), t.end());
 
-  return t;
+  // update each element in t
+  for (int i=0; i<n; i++) {
+    auto tMinus = newT; 
+    tMinus.erase(tMinus.begin()+i);
+
+    // create a map of unique t's
+    std::map<double,int> map_t_count;
+    for (int j=0; j<(n-1); j++) {
+      if (map_t_count.find( tMinus[j] ) != map_t_count.end()) 
+      { // if key exists
+        map_t_count[tMinus[j]]++;
+      } else {
+        map_t_count[tMinus[j]] = 1;
+      }
+    }
+    
+    double aux;
+    if (map_t_count.find(newT[i]) != map_t_count.end()) {
+      aux = rg0();
+    } else {
+      aux = newT[i];
+    }
+    double probAux = alpha * f(aux,i);
+
+    const int K = map_t_count.size();
+    double probExisting[K+1];
+    double unique_t[K+1];
+    
+    probExisting[0] = probAux;
+    unique_t[0] = aux;
+
+    int k=1;
+    for (auto const& ut : map_t_count) {
+      probExisting[k] = ut.second * f(ut.first,i);
+      unique_t[k] = ut.first;
+      k++;
+    }
+
+    newT[i] = unique_t[wsample_index(probExisting,K)];
+  }
+
+  // update by cluster
+  std::map<double,std::vector<int>> map_t_idx;
+  for (int i=0; i<n; i++) {
+    if (map_t_idx.find( newT[i] ) != map_t_idx.end()) { // if key exists
+      map_t_idx[newT[i]].push_back(i);
+    } else {
+      map_t_idx[newT[i]] = {i};
+    }
+  }
+  int k = 0;
+  for (auto const& ut : map_t_idx) {
+    auto idx = ut.second;
+    auto ll = [idx,lf](double tj) {
+      double out = 0;
+      for (int i=0; i<idx.size(); i++) {
+        out += lf(tj,idx[i]);
+      }
+      return out;
+    };
+    auto new_tj = mh(ut.first, ll, lg0, cs);
+    for (int i=0; i<idx.size(); i++) {
+      newT[i] = new_tj;
+    }
+  }
+
+  return NumericVector(newT.begin(), newT.end());
 }
+
+// need a wrapper for R. Then, test speed.

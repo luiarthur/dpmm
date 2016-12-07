d0786c76ac9aeedfdea4e9dc8cb25302c656cc72
Arthur Lui <luiarthur@gmail.com>
2016-12-07 11:16:38 -0800

    yes

---
+++ Cpp/more/Gibbs3.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+
+class GibbsState {
+  public:
+    virtual GibbsState* update() const = 0;
+
+    std::vector<GibbsState*> sample(int B, int burn, int printEvery) {
+      std::vector<GibbsState*> out(B);
+      out[0] = this;
+
+      for (int i=0; i<B+burn; i++) {
+        if (i <= burn) {
+          out[0] = out[0]->update();
+        } else {
+          out[i-burn] = out[i-burn-1]->update();
+        }
+
+        if (printEvery > 0 && i % printEvery == 0) {
+          std::cout << "\rProgress:  " << i << "/" << B+burn << "\t";
+        }
+      }
+      return out;
+    };
+}
+
+class State : public GibbsState {
+  public:
+
+    double mu;
+
+    State(double m) {mu = m;};
+
+    virtual State* update() const {
+      return new State(mu + 1);
+    };
+}
+
+
+auto s = new State(10);
+s->update()->mu
+auto x = s->sample(10,5,1)
+static_cast<State*>(x[0]) -> mu
+
+

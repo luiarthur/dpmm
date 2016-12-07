655032e2ab48080212f9ddc12528625d67299a45
Arthur Lui <luiarthur@gmail.com>
2016-12-07 01:53:59 -0800

    yes

--- Cpp/more/Gibbs2.cpp
+++ Cpp/more/Gibbs2.cpp
@@ -1,12 +1,8 @@
 #include<vector>
 #include<iostream>
 
-class GibbsState { public: virtual GibbsState* update() const = 0; }
-class State : public GibbsState {
+class State {
   public:
-    double p;
-    State(int p_in) {p = p_in;};
-    virtual State* update() const { return new State(p+2); };
     std::vector<State*> sample(int B, int burn, int printEvery) {
       std::vector<State*> out;
       out.reserve(B);
@@ -26,15 +22,11 @@ class State : public GibbsState {
 
       return out;
     }
+    // implement the following:
+    double mu;
+    State(double m) {mu = m;};
+    State* update() { return new State(mu + 1); }
 }
 
-auto s1 = new State(10);
-s1->p
-auto s2 = s1->update();
-s2->p = 20
-s1->p
-s2->p
-
-auto x = s1->sample(10,3,1)
-x[0] -> p
-x[9] -> p
+auto s = State(1)
+auto x = s.sample(100,10,10)

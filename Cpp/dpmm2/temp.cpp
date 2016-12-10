6eb4e9b140f02d07a6d339ba3ab0282a44800d03
Arthur Lui <luiarthur@gmail.com>
2016-12-10 14:19:04 -0800

    yes

--- Cpp/dpmm2/temp.cpp
+++ Cpp/dpmm2/temp.cpp
@@ -1,8 +1,31 @@
 #include <functional>
+#include <vector>
 
 template <class D>
-class GibbsState {
+class State {
   D data;
-  virtual GibbsState update() = 0;
-  virtual GibbsState sample(int B, int burn, int printEvery) = 0;
+  State update() = 0;
+  std::vector<State> sample(int B, int burn, int printEvery) {
+    std::vector<State> out(B);
+    out[0] = init;
+
+    for (int i=0; i<B+burn; i++) {
+      if (i <= burn) {
+        out[0] = out[0]->update();
+      } else {
+        out[i-burn] = out[i-burn-1]->update();
+      }
+
+      if (printEvery > 0 && (i+1) % printEvery == 0) {
+        Rcout << "\rProgress:  " << i+1 << "/" << B+burn << "\t";
+      }
+    }
+
+    Rcout << std::endl;
+    return out;
+  };
 };
+
+/*
+#include "temp.cpp"
+ */
